06ffb81af18509afb6e300f111b3f116c9fefb2b
Eugeny <info@coders.studio>
2019-08-15 13:38:25 +0300

    Cart test fix

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -25,6 +25,7 @@ class CartTest extends TestCase
      */
     public function testIndex():void
     {
+        Item::truncate();
         factory(Item::class, 10)->create();
         $this->addProduct(1);
         $this->addProduct(2);
@@ -33,11 +34,17 @@ class CartTest extends TestCase
         $response = $this->get('/cart');
         $response->assertStatus(200);
         $response->assertJson([
+            "meta" => [
+                "count" => 3,
+                "total" => 30
+            ],
             "data" => [
                 [
                     "type"=> "items",
                     "attributes"=> [
-                        "image_path" => "/test"
+                        "params" => [
+                            "image_path" => "/test"
+                        ]
                     ]
                 ]
             ]
@@ -51,8 +58,9 @@ class CartTest extends TestCase
      */
     public function testStore():void
     {
+        Item::truncate();
         factory(Item::class, 10)->create();
-        $response = $this->addProduct();
+        $response = $this->addProduct(1);
         $response->assertStatus(204);
     }
 
@@ -63,6 +71,7 @@ class CartTest extends TestCase
      */
     public function testDestroy()
     {
+        Item::truncate();
         factory(Item::class, 10)->create();
         $this->addProduct(1);
         $response = $this->json('delete', '/cart/1');
@@ -82,6 +91,7 @@ class CartTest extends TestCase
      */
     public function testClear()
     {
+        Item::truncate();
         factory(Item::class, 10)->create();
         $this->addProduct(1);
         $this->addProduct(2);
